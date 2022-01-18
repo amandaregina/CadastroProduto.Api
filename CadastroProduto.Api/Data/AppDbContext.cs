1431fb3e9a2b0d834e32c794064bdcdd560b7066
Amanda Regina Dos Santos Sales <amandaregina.s@hotmail.com>
2022-01-18 17:56:53 -0300

    Add project files.

---
+++ CadastroProduto.Api/Data/AppDbContext.cs
@@ -0,0 +1,54 @@
+﻿using CadastroProduto.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CadastroProduto.Api.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produto>()
+                .HasKey(p => p.Id);
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.Descricao)
+                    .IsRequired();
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.Codigo)
+                    .IsRequired();
+
+            modelBuilder.Entity<Produto>()
+                .HasIndex(p => p.Codigo)
+                    .IsUnique();
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.Preco)
+                    .HasPrecision(10, 2)
+                    .IsRequired();
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.Deletado)
+                    .IsRequired()
+                    .HasDefaultValue(false);
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.Status)
+                    .IsRequired()
+                    .HasDefaultValue(true);
+
+            modelBuilder.Entity<Produto>()
+                .HasData(
+                            new Produto { Id = Guid.Parse("120a0bd0-1ac0-8cd9-55c1-8ac501db680a"), Descricao = "Leite", Codigo = "1238585", Preco = 4, Deletado = false, Status = true },
+                            new Produto { Id = Guid.Parse("870c0bd0-1bc0-4cd2-39c2-2ac551dc681c"), Descricao = "Manteiga", Codigo = "555566", Preco = 6, Deletado = false, Status = true });
+        }
+    }
+}
---
+++ CadastroProduto.Api/Models/Produto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CadastroProduto.Api.Models
+{
+    public class Produto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        public string Codigo { get; set; }
+
+        public string Descricao { get; set; }
+
+        public List<Departamento> Departamento { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public bool Status { get; set; }
+
+        public bool Deletado { get; set; }
+    }
+}
