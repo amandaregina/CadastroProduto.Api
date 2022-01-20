45b7c39902ee91d6294057a47fa18efdf3715419
Amanda Regina Dos Santos Sales <amandaregina.s@hotmail.com>
2022-01-19 21:13:46 -0300

    Web app

---
+++ CadastroProduto.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,22 @@
+﻿using CadastroProduto.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CadastroProduto.Web.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            IEnumerable<Produto> produtoList;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Produto").Result;
+            produtoList = response.Content.ReadAsAsync<IEnumerable<Produto>>().Result;
+
+            return View(produtoList);
+        }
+    }
+}
---
+++ CadastroProduto.Web/Models/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace CadastroProduto.Web.Models
+{
+    public class Produto
+    {
+        public Guid Id { get; set; }
+
+        public string Codigo { get; set; }
+
+        public string Descricao { get; set; }
+
+        public List<Departamento> Departamento { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public bool Status { get; set; }
+
+        public bool Deletado { get; set; }
+    }
+}
